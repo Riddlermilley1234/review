2efab4f3245819c2f9d3e1595f8b9f7e588c1691
Riddlermilley1234 <onisamuel873@gmail.com>
2024-07-26 07:38:41 -0700

    Add files via upload

---
+++ review.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper ", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
